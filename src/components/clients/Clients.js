e837f2fad2a6d2153606b03c3afb2619d4c299ae
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-02 14:04:44 +0200

    adds example clients, adds table in Clients component with example clients

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Clients extends Component {
   render() {
-    return (
-      <div>
-        <h1>Clients</h1>
-      </div>
-    )
+    const clients = [{
+      id: '43434343',
+      firstName: 'Kevin',
+      lastName: 'Johnson',
+      email: 'kevin@johnson.com',
+      phone: '555-555-5555',
+      balance: '30'
+    },
+    {
+      id: '22222222',
+      firstName: 'Bob',
+      lastName: 'Jackson',
+      email: 'bob@jackson.com',
+      phone: '222-555-5555',
+      balance: '1000.50'
+    }
+  ]
+
+    if(clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                {' '}
+                <i className="fas fa-users"></i> Clients {' '}</h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>{client.firstName} {client.lastName}</td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link to={`/clients/${client.id}`} className="btn btn-secondary btn-sm">
+                      <i className="fas fa-arrow-circle-right"></i> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading..</h1>
+    }
   }
 }
 
