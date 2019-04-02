fec6695f527a8e2ee6b49ba284e4000b453c8557
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-02 23:19:42 +0200

    install redux-firebase due to earlier typo ($ npm install redux-firestore), setting up firebase with boilerplates in store file, adds .env file for eg api-key, adds .env to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <AppNavbar />
-          <div className="container">          
-            <Switch>
-              <Route exact path="/" component={Dashboard} />
-            </Switch>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <AppNavbar />
+            <div className="container">          
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ src/store.js
@@ -0,0 +1,51 @@
+import { createStore, combineReducers, compose } from 'redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+
+// Reducers
+// todo
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+}
+
+// Init firebase instance
+firebase.initializeApp(firebaseConfig);
+// Init firestore
+// const firestore = firebase.firestore();
+
+// Add react-redux-firebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig),
+  reduxFirestore(firebase)
+)(createStore);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+// Create intial state
+const initialState = {};
+
+// Create store
+const store = createStoreWithFirebase(rootReducer, initialState, compose(
+  reactReduxFirebase(firebase),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export default store;
