98afd85f4e7c37972844e377c751101aa2ab68d0
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-03 23:01:00 +0200

    adds the form for editing client info

--- src/components/clients/EditClient.js
+++ src/components/clients/EditClient.js
@@ -7,14 +7,94 @@ import { firestoreConnect } from 'react-redux-firebase';
 import Spinner from '../layout/Spinner';
 
 class EditClient extends Component {
+
+
   render() {
     const { client } = this.props;
 
     if(client) {
       return (
         <div>
-          <h1>{client.firstName}</h1>
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/" className="btn btn-link">
+              <i className="fas fa-arrow-circle-left"></i> Back To Dashboard
+            </Link>
+          </div>
         </div>
+
+        <div className="card">
+          <div className="card-header">Edit Client</div>
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input 
+                  type="text" 
+                  className="form-control"
+                  name="firstName"
+                  minLength="2"
+                  required
+                  ref={this.firstNameInput}
+                  defaultValue={client.firstName}
+                  />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <input 
+                  type="text" 
+                  className="form-control"
+                  name="lastName"
+                  minLength="2"
+                  required
+                  ref={this.lastNameInput}
+                  defaultValue={client.lastName}
+                  />
+              </div>
+              
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input 
+                  type="email" 
+                  className="form-control"
+                  name="email"
+                  ref={this.emailInput}
+                  defaultValue={client.email}
+                  />
+              </div>
+             
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input 
+                  type="text" 
+                  className="form-control"
+                  name="phone"
+                  minLength="10"
+                  required
+                  ref={this.phoneInput}
+                  defaultValue={client.phone}
+                  />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="balance">Balance</label>
+                <input 
+                  type="text" 
+                  className="form-control"
+                  name="balance"
+                  ref={this.balanceInput}                 
+                  defaultValue={client.balance}
+                  />
+              </div>
+
+              <input type="submit" value="Submit" className="btn btn-primary btn-block"/>
+
+            </form>
+          </div>
+        </div>
+
+      </div>
       )
     } else {
       return <Spinner />
