a84c24cc99e560e8823699e9595f98ab566d23df
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-03 18:59:05 +0200

    adds form with client info, installing classnames ($ npm i classnames), setting amount to be red when owing money and green if not

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import Spinner from '../layout/Spinner';
+import classnames from 'classnames';
 
 class ClientDetails extends Component {
   render() {
@@ -27,13 +28,45 @@ class ClientDetails extends Component {
                 <button className="btn btn-danger">Delete</button>
               </div>
             </div>
-          </div>          
+          </div>
+          <hr/>
+          <div className="card">
+            <h3 className="card-header">
+              {client.firstName} {client.lastName}
+            </h3>
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-8 col-sm-6">
+                  <h4>Client ID:{' '} <span className="text-secondary">{client.id}</span></h4>
+                </div>
+                <div className="col-md-4 col-sm-6">
+                  <h3 className="pull-right">                 
+                    Balance: <span className={classnames({
+                      'text-danger': client.balance > 0,
+                      'text-success': client.balance === 0
+                    })}
+                    >${parseFloat(client.balance).toFixed(2)}</span>
+                  </h3>
+                  {/* @todo - balance form */}
+                </div>
+              </div>
+              <hr/>
+              <ul className="list-group">
+                <li className="list-group-item">Contact Email: {client.email}</li>
+                <li className="list-group-item">Contact Phone: {client.phone}</li>
+              </ul>
+            </div>
+          </div>        
         </div>
       );
     } else {
       return <Spinner />;
     }
   }
+}
+
+ClientDetails.propTypes = {
+  firestore: PropTypes.object.isRequired
 }
 
 export default compose(
