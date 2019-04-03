fb144b8dada905e2149c7e2b61b877f0407822ac
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-03 20:00:43 +0200

    adds icon to change balance and a form and submit btn to make the change (not to firestore yet)

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -8,8 +8,45 @@ import Spinner from '../layout/Spinner';
 import classnames from 'classnames';
 
 class ClientDetails extends Component {
+  state = {
+    showBalanceUpdate: false,
+    balanceUpdateAmount: ''
+  }
+
+  balanceSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.balanceUpdateAmount);
+  }
+
+  onChange = e => this.setState({[e.target.name]: e.target.value});
+
   render() {
     const { client } = this.props;
+    const { showBalanceUpdate, balanceUpdateAmount } = this.state;
+
+    let balanceForm = '';
+    // If balance form should display
+    if(showBalanceUpdate) {
+      balanceForm = (
+        <form onSubmit={this.balanceSubmit}>
+          <div className="input-group">
+            <input 
+              type="text" 
+              className="form-control"
+              name="balanceUpdateAmount"
+              placeholder="Add New Balance"
+              value={balanceUpdateAmount}
+              onChange={this.onChange}
+            />
+            <div className="input-group-append">
+              <input type="submit" value="Update" className="btn btn-outline-dark"/>
+            </div>
+          </div>
+        </form>
+      )
+    } else {
+      balanceForm = null;
+    }
 
     if(client) {
       return (
@@ -41,13 +78,21 @@ class ClientDetails extends Component {
                 </div>
                 <div className="col-md-4 col-sm-6">
                   <h3 className="pull-right">                 
-                    Balance: <span className={classnames({
+                    Balance: 
+                    <span className={classnames({
                       'text-danger': client.balance > 0,
                       'text-success': client.balance === 0
                     })}
-                    >${parseFloat(client.balance).toFixed(2)}</span>
+                    >
+                      ${parseFloat(client.balance).toFixed(2)}
+                    </span>
+                    <small>
+                      <a href="#!" onClick={() => this.setState({ showBalanceUpdate: !this.state.showBalanceUpdate })}>
+                        <i className="fas fa-pencil-alt"></i>
+                      </a>
+                    </small>
                   </h3>
-                  {/* @todo - balance form */}
+                  {balanceForm}
                 </div>
               </div>
               <hr/>
