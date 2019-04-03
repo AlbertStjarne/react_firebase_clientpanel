f32b12a0058e3eae6c4fd5d2117e8b858bc46fad
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-03 16:55:03 +0200

    adds ClientDetails component, fetciohing client to the clients page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import store from './store';
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
+import ClientDetails from './components/clients/ClientDetails';
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
+                <Route exact path="/client/:id" component={ClientDetails} />
               </Switch>
             </div>
           </div>
---
+++ src/components/clients/ClientDetails.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+//import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import Spinner from '../layout/Spinner';
+
+class ClientDetails extends Component {
+  render() {
+    const { client } = this.props;
+
+    if(client) {
+      return (
+        <div>
+          <h1>{client.firstName}</h1>
+          
+        </div>
+      );
+    } else {
+      return <Spinner />;
+      //{console.log('bye')}
+    }
+  }
+}
+
+export default compose(
+  firestoreConnect(props => [
+    { collection: 'clients', storeAs: 'client', doc: props.match.params.id }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0]
+  }))
+)(ClientDetails);
