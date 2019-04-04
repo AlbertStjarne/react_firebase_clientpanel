e33ce98d0f2943ebcfaa8e28d0ab4381dea1a4dd
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-04 17:52:22 +0200

    disabling to edit balance when entering a new client depending on the toggle in settings

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-// import { compose } from 'redux';
-// import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 
 class AddClient extends Component {
@@ -32,6 +32,8 @@ class AddClient extends Component {
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { disableBalanceOnAdd } = this.props.settings;
+
     return (
       <div>
         <div className="row">
@@ -104,6 +106,7 @@ class AddClient extends Component {
                   name="balance"                  
                   onChange={this.onChange}
                   value={this.state.balance}
+                  disabled={disableBalanceOnAdd}
                   />
               </div>
 
@@ -119,7 +122,13 @@ class AddClient extends Component {
 }
 
 AddClient.propTypes = {
-  firestore: PropTypes.object.isRequired
+  firestore: PropTypes.object.isRequired,
+  settings: PropTypes.object.isRequired
 }
 
-export default firestoreConnect()(AddClient);
+export default compose(
+  firestoreConnect(),
+  connect((state, props) => ({
+    settings: state.settings
+  }))
+)(AddClient);
