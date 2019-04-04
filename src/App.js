6cfb5e4ef8c7108abfa4795c63a3b8486feba9fb
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-04 11:08:51 +0200

    adds auth functionality, importing UserIsAuthenticated and UserIsNotAuthenticated and adding them to the routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth';
 import './App.css';
 
 import { Provider } from 'react-redux';
@@ -21,11 +22,11 @@ class App extends Component {
             <AppNavbar />
             <div className="container">          
               <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/edit/:id" component={EditClient} />
-                <Route exact path="/client/:id" component={ClientDetails} />
-                <Route exact path="/login" component={Login} />
+                <Route exact path="/" component={UserIsAuthenticated(Dashboard)} />
+                <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
+                <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+                <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
+                <Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
               </Switch>
             </div>
           </div>
