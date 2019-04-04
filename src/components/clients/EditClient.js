e795874906027977a1a2129f124991b8e7e5dc2d
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-04 17:56:35 +0200

    disabling to edit balance when editing an existing client

--- src/components/clients/EditClient.js
+++ src/components/clients/EditClient.js
@@ -38,6 +38,7 @@ class EditClient extends Component {
 
   render() {
     const { client } = this.props;
+    const { disableBalanceOnEdit } = this.props.settings;
 
     if(client) {
       return (
@@ -112,6 +113,7 @@ class EditClient extends Component {
                   name="balance"
                   ref={this.balanceInput}                 
                   defaultValue={client.balance}
+                  disabled={disableBalanceOnEdit}
                   />
               </div>
 
@@ -137,7 +139,8 @@ export default compose(
   firestoreConnect(props => [
     { collection: 'clients', storeAs: 'client', doc: props.match.params.id }
   ]),
-  connect(({ firestore: { ordered } }, props) => ({
-    client: ordered.client && ordered.client[0]
+  connect(({ firestore: { ordered }, settings }, props) => ({
+    client: ordered.client && ordered.client[0],
+    settings: settings
   }))
 )(EditClient);
