94cf9180127e67c15e35c7a721f133994f5e328a
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-04 13:13:40 +0200

    adds notifyActions file, adds content, implement in Login by outputting Alert message

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { compose } from 'redux';
-// import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { firebaseConnect } from 'react-redux-firebase';
+import { notifyUser } from '../../actions/notifyActions';
+import Alert from '../layout/Alert';
 
 class Login extends Component {
   state = {
@@ -13,23 +15,27 @@ class Login extends Component {
   onSubmit = e => {
     e.preventDefault();
 
-    const { firebase } = this.props;
+    const { firebase, notifyUser } = this.props;
     const { email, password } = this.state;
 
     firebase.login({
       email,
       password
-    }).catch(err => alert('Invalid login credentials'));
+    }).catch(err => notifyUser('Invalid login credentials', 'error'));
   }
 
   onChange = e => this.setState({[e.target.name]: e.target.value});
 
   render() {
+    const { message, messageType } = this.props.notify;
     return (
       <div className="row">
         <div className="col-md-6 mx-auto">
           <div className="card">
             <div className="card-body">
+              {message ? (
+                <Alert message={message} messageType={messageType} />
+              ) : null}
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
                   <i className="fas fa-lock"></i>{' '}
@@ -74,4 +80,10 @@ class Login extends Component {
 Login.propTypes = {
   firebase: PropTypes.object.isRequired
 }
-export default firebaseConnect()(Login);
+export default compose(
+  firebaseConnect(),
+  connect((state, props) => ({
+    notify: state.notify
+  }), { notifyUser }
+)
+)(Login);
--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -2,17 +2,18 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Alert = (props) => {
-  const { message, messageType } = this.props;
+const Alert = props => {
+  const { message, messageType } = props;
   return (
     <div className={classnames('alert', {
       'alert-success': messageType === 'success',
       'alert-danger': messageType === 'error'
-    })}>
+    })}
+    >
       {message}
     </div>
-  )
-}
+  );
+};
 
 Alert.propTypes = {
   message: PropTypes.string.isRequired,
