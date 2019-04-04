09ef1bebf0e2ad1cdec5dbfde9946c7ca2fc5281
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-04 17:43:06 +0200

    persist settings to localStorage

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,28 +1,25 @@
-import { DISABLE_BALANCE_ON_ADD, DISABLE_BALANCE_ON_EDIT, ALLOW_REGISTRATION } from '../actions/types';
-import { actionTypes } from '../../node_modules/react-redux-firebase';
+import { 
+  DISABLE_BALANCE_ON_ADD, 
+  DISABLE_BALANCE_ON_EDIT, 
+  ALLOW_REGISTRATION 
+} from '../actions/types';
 
-const initialState = {
-  disableBalanceOnAdd: true,
-  disableBalanceOnEdit: false,
-  allowRegistration: false
-}
-
-export default function(state = initialState) {
-  switch(actionTypes.type) {
+export default function(state = {}, action) {
+  switch(action.type) {
     case DISABLE_BALANCE_ON_ADD:
       return {
         ...state,
-        disableBalanceOnAdd: !state.disableBalanceOnAdd
+        disableBalanceOnAdd: action.payload
       };
     case DISABLE_BALANCE_ON_EDIT:
       return {
         ...state,
-        disableBalanceOnEdit: !state.disableBalanceOnEdit
+        disableBalanceOnEdit: action.payload
       };
     case ALLOW_REGISTRATION:
       return {
         ...state,
-        allowRegistration: !state.allowRegistration
+        allowRegistration: action.payload
       };
     default:
       return state;
