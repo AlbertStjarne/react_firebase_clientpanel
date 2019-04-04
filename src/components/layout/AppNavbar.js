c64408280a5c1bcd3ea7b30041dac82efc2ab9af
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-04 18:07:04 +0200

    if allow registration is clicked in settings, having a register link in the navbar

--- src/components/layout/AppNavbar.js
+++ src/components/layout/AppNavbar.js
@@ -30,6 +30,7 @@ class AppNavbar extends Component {
   render() {
     const { isAuthenticated } = this.state;
     const { auth } = this.props;
+    const { allowRegistration } = this.props.settings;
 
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
@@ -70,6 +71,21 @@ class AppNavbar extends Component {
                     >
                       Logout  
                     </a>
+                  </li>
+                </ul>
+              ) : null}
+
+              {allowRegistration && !isAuthenticated ? (
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link to="/login" className="nav-link">
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/register" className="nav-link">
+                      Register
+                    </Link>
                   </li>
                 </ul>
               ) : null}
